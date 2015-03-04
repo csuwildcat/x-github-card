9a1f28c70effaebd99bc097911cb0e3f062cc2e4
Daniel Buchner <danieljb2@gmail.com>
2015-03-04 14:45:56 -0800

    Switched to X-Tag recommended content population style

--- src/main.js
+++ src/main.js
@@ -17,70 +17,38 @@
   }
 
   xtag.register('x-github-card', {
-    lifecycle: {
-      created: function() {
-        // Create HTML tags
-        this.xtag.user = document.createElement('article');
-        this.xtag.userData = document.createElement('dl');
-        this.xtag.userDataDt = document.createElement('dt');
-        this.xtag.userAvatar = document.createElement('dd');
-        this.xtag.userAvatarImg = document.createElement('img');
-        this.xtag.userName = document.createElement('dd');
-        this.xtag.userNameDt = document.createElement('dt');
-        this.xtag.userAccount = document.createElement('dd');
-        this.xtag.userAccountDt = document.createElement('dt');
-        this.xtag.userStats = document.createElement('dl');
-        this.xtag.userRepos = document.createElement('dd');
-        this.xtag.userReposDt = document.createElement('dt');
-        this.xtag.userFollowers = document.createElement('dd');
-        this.xtag.userFollowersDt = document.createElement('dt');
-
-        // Set classes
-        this.xtag.user.className = 'user';
-        this.xtag.userData.className = 'user-data';
-        this.xtag.userDataDt.innerHTML = 'Avatar:';
-        this.xtag.userAvatar.className = 'user-avatar';
-        this.xtag.userAvatarImg.className = 'avatar-img';
-        this.xtag.userStats.className = 'user-stats';
-        this.xtag.userRepos.className = 'user-repos';
-        this.xtag.userRepos.setAttribute('data-stats', 'repos');
-        this.xtag.userReposDt.innerHTML = 'Repos';
-        this.xtag.userFollowers.className = 'user-followers';
-        this.xtag.userFollowers.setAttribute('data-stats', 'followers');
-        this.xtag.userFollowersDt.innerHTML = 'Followers';
-        this.xtag.userName.className = 'user-name';
-        this.xtag.userNameDt.innerHTML = 'Fullname:';
-        this.xtag.userAccount.className = 'user-account';
-        this.xtag.userAccountDt.innerHTML = 'Account:';
-
-        // let's append
-        this.appendChild(this.xtag.user);
-        this.xtag.user.appendChild(this.xtag.userData);
-        this.xtag.user.appendChild(this.xtag.userStats);
-        this.xtag.userData.appendChild(this.xtag.userDataDt);
-        this.xtag.userData.appendChild(this.xtag.userAvatar);
-        this.xtag.userAvatar.appendChild(this.xtag.userAvatarImg);
-        this.xtag.userData.appendChild(this.xtag.userNameDt);
-        this.xtag.userData.appendChild(this.xtag.userName);
-        this.xtag.userData.appendChild(this.xtag.userAccountDt);
-        this.xtag.userData.appendChild(this.xtag.userAccount);
-        this.xtag.userStats.appendChild(this.xtag.userReposDt);
-        this.xtag.userStats.appendChild(this.xtag.userRepos);
-        this.xtag.userStats.appendChild(this.xtag.userFollowersDt);
-        this.xtag.userStats.appendChild(this.xtag.userFollowers);
-      }
-    },
+    content: function(){/*
+      <article class="user">
+        <dl class="user-data">
+          <dt>Avatar:</dt>
+          <dd class="user-avatar">
+            <img class="avatar-img" />
+          </dd>
+          <dt>Fullname:</dt>
+          <dd class="user-name"></dd>
+          <dt>Account:</dt>
+          <dd class="user-account"></dd>
+        </dl>
+        <dl class="user-stats">
+          <dt>Repos</dt>
+          <dd data-stats="repos" class="user-repos"></dd>
+          <dt>Followers</dt>
+          <dd data-stats="followers" class="user-followers"></dd>
+        </dl>
+      </article>
+    */},
     accessors: {
       user: {
         attribute: {}, // don't need to specify the name, it's automatically linked
         // don't need to specify a 'get' function, as this is already linked as well
         set: function(val){ // you probably want to switch the card when the 'user' attr is changed
+          var card = this;
           json(url, val, function(response) {
-            document.querySelector('.user-name').innerHTML = response.name;
-            document.querySelector('.avatar-img').setAttribute('src', response.avatar_url);
-            document.querySelector('.user-account').innerHTML = response.login;
-            document.querySelector('.user-repos').innerHTML = response.public_repos;
-            document.querySelector('.user-followers').innerHTML = response.followers;
+            card.querySelector('.user-name').innerHTML = response.name;
+            card.querySelector('.avatar-img').setAttribute('src', response.avatar_url);
+            card.querySelector('.user-account').innerHTML = response.login;
+            card.querySelector('.user-repos').innerHTML = response.public_repos;
+            card.querySelector('.user-followers').innerHTML = response.followers;
           });
         }
       }
